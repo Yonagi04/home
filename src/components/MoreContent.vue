77af9d78a5126591c898236bd5ef12c63dd4187b
Yonagi <1809266700@qq.com>
2024-10-29 14:50:28 +0800

    update: add site

--- src/components/MoreContent.vue
+++ src/components/MoreContent.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="more-content">您可在此编写任意内容</div>
+  <div class="more-content">Even if I can't break it down, I will survive this moment on this stage.</div>
 </template>
 
 <style lang="scss" scoped>
